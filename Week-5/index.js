61e15cbcb3108befc19fa829cb912cff980f93f7
Leslie Mejia <lesliemejia110316@gmail.com>
2024-10-23 11:34:35 -0700

    learned about variables and operators

--- Week-5/index.js
+++ Week-5/index.js
@@ -1,6 +1,6 @@
 // console.log("hello world")
 // alert('Hello, world!');
-window.document.write('<p>Welcome to CSEN 60!</p>');
+//window.document.write('<p>Welcome to CSEN 60!</p>');
 
 // document.append ('<p>This should come after the h1</p>')
 
@@ -21,3 +21,48 @@ function add(a, b) {
     return a + b;
 }
 
+// var example
+let x = 100;
+//const x -> declaring variable
+//x = 2 -> initializing 
+if (true) {
+    x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+let y;
+y = null;
+let z = 0;
+console.log(y,z);
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date()}`;
+console.log(response);
+
+const age = 15;
+if (age > 18) {
+    console.log("You are of age")
+}
+else{
+    console.log(`You arent of age. Please wait ${18-age} years`)
+}
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person["age"])
+
+console.log(typeof("hello world"))
+
+alert("This is an important message");
+
+// Strict equality -- 1. Are these the same type? 2. Are these the same value?
+console.log(3 === '3'); // false
+
+// Loose equality -- Are these the same value?
+console.log(3 == '3'); // true
+
+let a = 2 // initializing variable 
