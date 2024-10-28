4c648dbea7310ca939a448427be20713edc6ba72
Leslie Mejia <lesliemejia110316@gmail.com>
2024-10-28 11:35:09 -0700

    create addtodo function

---
+++ Week-6/index.js
@@ -0,0 +1,16 @@
+/**
+ * takes input ...
+ */
+function addTodo() {
+    console.log ("button was clicked")
+    const input = document.getElementById("list-input")
+    console.log("input.value =",input.value)
+
+    const ul = document.querySelector("ul")
+    // const list = lists[0]
+    console.log (ul)
+
+    const li = document.createElement("li")
+    li.textContent =input.value
+    ul.appendChild(li)
+}
